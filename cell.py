5c44c84ebca0417065a06f58ee48310f6735cc81
Jaroslav Hůna <jaroslav.huna@outlook.cz>
2025-02-01 15:44:03 +0100

    tests

--- cell.py
+++ cell.py
@@ -2,7 +2,7 @@ from graphics import Line, Point
 
 
 class Cell:
-    def __init__(self, win, **kwargs):
+    def __init__(self, win=None, **kwargs):
         self.has_left_wall = kwargs.get('has_left_wall', True)
         self.has_right_wall = kwargs.get('has_right_wall', True)
         self.has_top_wall = kwargs.get('has_top_wall', True)
@@ -16,7 +16,7 @@ class Cell:
     def draw(self, **kwargs):
         if self._win is None:
             return
-        
+
         self._x1 = kwargs.get('x1', 0)
         self._y1 = kwargs.get('y1', 0)
         self._x2 = kwargs.get('x2', 0)
@@ -26,25 +26,25 @@ class Cell:
             left_wall = Line(
                 Point(self._x1, self._y1),
                 Point(self._x1, self._y2)
-                )
+            )
             self._win.draw_line(left_wall)
         if self.has_right_wall:
             right_wall = Line(
                 Point(self._x2, self._y2),
                 Point(self._x2, self._y1)
-                )
+            )
             self._win.draw_line(right_wall)
         if self.has_top_wall:
             top_wall = Line(
                 Point(self._x1, self._y1),
                 Point(self._x2, self._y1)
-                )
+            )
             self._win.draw_line(top_wall)
         if self.has_bottom_wall:
             bottom_wall = Line(
                 Point(self._x2, self._y2),
                 Point(self._x1, self._y2)
-                )
+            )
             self._win.draw_line(bottom_wall)
 
     def draw_move(self, to_cell, undo=False):
@@ -56,7 +56,6 @@ class Cell:
         else:
             fill_color = "gray"
         self._win.draw_line(path, fill_color=fill_color)
-
 
     def get_center_coordinates(self):
         x = self._x1 + (abs(self._x2 - self._x1) // 2)
--- main.py
+++ main.py
@@ -2,34 +2,19 @@ from graphics import Window, Point, Line
 from cell import Cell
 from maze import Maze
 
+
 def main():
-    win = Window(800, 600)
-    
-    # c1 = Cell(win)
-    # c1.has_left_wall = False
-    # c1.draw(x1=50, y1=50, x2=100, y2=100)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
 
-    # c2 = Cell(win)
-    # c2.has_right_wall = False
-    # c2.draw(x1=125, y1=125, x2=200, y2=200)
-
-    # c1.draw_move(c2)
-
-    # c3 = Cell(win)
-    # c3.has_bottom_wall = False
-    # c3.draw(x1=225, y1=225, x2=250, y2=250)
-
-    # c2.draw_move(c3)
-
-    # c4 = Cell(win)
-    # c4.has_top_wall = False
-    # c4.draw(x1=300, y1=300, x2=500, y2=500)
-
-    # c3.draw_move(c4, True)
-
-    maze = Maze(25,25,18,24,25,25,win)
-    maze._create_cells()
-
+    maze = Maze(margin, margin, num_rows, num_cols,
+                cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 
